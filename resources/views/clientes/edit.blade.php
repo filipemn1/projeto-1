f02538b928c9b3a89152a86b7e9cbc0fb45731b0
filipemn1 <41974153+filipemn1@users.noreply.github.com>
2018-08-07 17:43:03 +0100

    Update edit.blade.php

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -1,40 +1,118 @@
-@extends('adminlte::layouts.app')
-@section('contentheader_title')
-    Perfil do Cliente
-@endsection
-24
-@section('main-content')
-    <section class="content">
-        <div class="row">
-            <div class="col-md-3">
-                <!-- Profile Image -->
-                <div class="box box-primary">
-                    <div class="box-body box-profile">
-                        <img class="profile-user-img img-responsive img-circle"
-                             src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
-                        <h3 class="profile-username text-center">{{ $cliente->nome }}</h3>
+<?php
 
-                        <ul class="list-group list-group-unbordered">
-                            <li class="list-group-item">
-                                <b>Nif</b> <a class="pull-right">{{ $cliente->nif }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Morada</b> <a class="pull-right">{{ $cliente->morada }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Nº tlm</b> <a class="pull-right">{{ $cliente->telemovel }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Email</b> <a class="pull-right">{{ $cliente->email }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
+namespace App\Http\Controllers;
 
-            </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
-    </section>
-@endsection
+use Illuminate\Http\Request;
+use App\Cliente;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\Redirect;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('clientes.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clientes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $cliente= new Cliente();
+        $cliente->nome = $request->nome;
+        $cliente->nif = $request->nif;
+        $cliente->morada = $request->morada;
+        $cliente->telemovel = $request->telemovel;
+        $cliente->email = $request->email;
+
+        $cliente->save();
+
+        return Redirect::to('gestao/clientes/create');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cliente= Cliente::find($id);
+        return view('clientes.edit', ['cliente' => Cliente::findOrFail($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cliente= Cliente::findOrFail($id);
+        $cliente->update($request->all());
+        return Redirect::to('gestao/clientes/'.$cliente->id.'/edit');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getClientes()
+    {
+        return Datatables::of(Cliente::query())
+            ->addColumn('encomendar', function ($cliente) {
+                return '<a href="{{ URL::to(\'gestao/clientes/encomendar\')}}" <button type="button" class="btn btn-block btn-primary"></i> Encomendar</a>';
+            })
+            ->addColumn('estado', function ($cliente) {
+                return '<a href="clientes/' . $cliente->id . '/edit" <button type="button" class="btn btn-block btn-primary"></i> Perfil</a>';
+            })
+            ->addColumn('action', function ($cliente) {
+                return '<a href="{{ route(fornecedores.destroy ,$fornecedor->id)}}" class="btn btnprimary
+tooltips"><i class="fa fa-close fa fa-white"></i></a>';
+            })
+            ->rawColumns(['encomendar','estado','action'])
+            ->make(true);
+
+    }
+}
